d2f652990538025b17e6c56699f24217108666e5
tammyn102 <65318171+tammyn102@users.noreply.github.com>
2021-10-08 12:36:54 -0600

    new add to frontend

--- signIn.js
+++ signIn.js
@@ -1,13 +1,14 @@
+var userType = "";
 
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    // window.location = 'http://127.0.0.1:5500/studentview.html'// redirect to another link
-    var email = profile.getEmail()
-    var id = profile.getId()
+    // var profile = googleUser.getBasicProfile();
+    // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    // console.log('Name: ' + profile.getName());
+    // console.log('Image URL: ' + profile.getImageUrl());
+    // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    // // window.location = 'http://127.0.0.1:5500/studentview.html'// redirect to another link
+    // var email = profile.getEmail()
+    // var id = profile.getId()
 
     var id_token = googleUser.getAuthResponse().id_token;
 
@@ -22,7 +23,8 @@ function onSignIn(googleUser) {
         })
             .then(response => {
                 console.log(response.data)
-                var userType = response.data
+                userType = response.data
+                
         
 if (userType == "Professor")
 {
@@ -41,15 +43,5 @@ else if (userType == "Student")
             } )
     }
     postItem()
-//this function is a if statement that decides if the user is student/teacher
-  
 }
-
-// this function sign out of the website
-// function signOut() {
-//     var auth2 = gapi.auth2.getAuthInstance();
-//     auth2.signOut().then(function () {
-//         console.log('User signed out.')
-//     });
-// }
-
+  
