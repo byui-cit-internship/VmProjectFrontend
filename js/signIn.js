9f7ea9e21328b5119b6cc4cd49c31cf780047035
Lakeram Narine <nar19002@byui.edu>
2021-12-02 15:57:16 -0700

    added comments for faculty view

--- js/signIn.js
+++ js/signIn.js
@@ -1,40 +1,33 @@
-// This is the variable that is being call from the backend
-var userType = "";
-
+/**********************************************
+ * This function triggers the google third party api to auth our user, it helps to create
+ * our sign-in page so we don't have to create one.
+ * ******************************** */
 function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  // console.log("Basic profile: " + JSON.stringify(profile))
-  // console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  // console.log("Name: " + profile.getName());
-  // console.log("Image URL: " + profile.getImageUrl());
-  // console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
-  var email = profile.getEmail();
-  var id = profile.getId();
   var id_token = googleUser.getAuthResponse().id_token;
   console.log(id_token);
-  // we are using axios to get the data from the backend to the frontend
+
+  // Making a call to the back end to verify token and check wheather user exits in database 
+  //they are a professor or student.
   const postItem = () => {
     axios({
       method: "get",
       url: "https://localhost:5001/api/token/" + id_token,
 
     }).then((response) => {
-      // console.log("whatever", response.data);
       user = response.data;
       console.log(user)
-      // // we are filtering the  userType to verify if they are a student or professor
+      //we are filtering the  userType to verify if they are a student or professor
       if (user == "Professor") {
         window.location.href = "/FrontEnd-VMproject/facultyview.html#" + id_token;
       } else if (user == "Student") {
         window.location.href = "/FrontEnd-VMproject/studentview.html#" + id_token;
         // +user.userID;
       } else {
-        //  window.location.href = "/";
+        window.location.href = "/";
       }
     });
   };
   postItem();
-
   savetoken(id_token);
 }
 
