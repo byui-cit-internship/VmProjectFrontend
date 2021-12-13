27ca1ed920cdedbc9a8c680fa728c5133b0c99b8
Lakeram Narine <nar19002@byui.edu>
2021-12-13 10:46:15 -0700

    added the getApiUrl

--- VMfaculty_dashboard/addadminportal.js
+++ VMfaculty_dashboard/addadminportal.js
@@ -2,13 +2,13 @@
 const isAdmin = sessionStorage.getItem("isAdmin")
 console.log("here", isAdmin)
 const createAdmin = () => {
-    if (isAdmin == "true") {
-        const portal_div = document.getElementById("admin_portal")
-        let aTag = document.createElement('a');
-        aTag.setAttribute('href', "/VMAdmin_dash/admin.html");
-        aTag.innerHTML = "Admin Portal";
-        aTag.setAttribute("class", "submitBt btn-primary")
-        portal_div.append(aTag)
-    }
+
+    const portal_div = document.getElementById("admin_portal")
+    let aTag = document.createElement('a');
+    aTag.setAttribute('href', "/VMAdmin_dash/admin.html");
+    aTag.innerHTML = "Admin Portal";
+    aTag.setAttribute("class", "submitBt btn-primary")
+    portal_div.append(aTag)
+
 }
 createAdmin()
