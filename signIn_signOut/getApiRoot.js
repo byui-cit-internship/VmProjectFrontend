938663aa85b93f06c2c3a1901abb40e3c1f4f145
sarii96 <sarah-96325@hotmail.com>
2022-01-06 14:17:05 -0700

    getApiChanges

--- signIn_signOut/getApiRoot.js
+++ signIn_signOut/getApiRoot.js
@@ -1,9 +1,10 @@
 export const getApiRoot = () => {
     const hashTag = window.location.hostname;
     console.log('Hash tag ' + hashTag);
-    let apiRoot = hashTag === 'localhost'
-        ? 'https://localhost:5001'
-        : 'http://dev-vm-api.citwdd.net';
+    let apiRoot = 'http://dev-vm-api.citwdd.net';
+    // hashTag === 'localhost'
+        // ? 'https://localhost:5001'
+        // : 'http://dev-vm-api.citwdd.net';
 
     if (window.location.hostname.includes('dev-vm')) {
         apiRoot = 'https://dev-vm-api.citwdd.net';
