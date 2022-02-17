dd632c508b277a32b763330a83f90fa20ecac089
mebenal <willtreatyaraluen@gmail.com>
2022-02-17 14:17:11 -0700

    Fixed urls

--- signIn_signOut/getApiRoot.js
+++ signIn_signOut/getApiRoot.js
@@ -3,20 +3,21 @@ export const getApiRoot = () => {
     console.log('Hash tag ' + hashTag);
     let apiRoot = 'http://localhost:5000';
     // hashTag === 'localhost'
-        // ? 'https://localhost:5001'
-        // : 'http://dev-vm-api.citwdd.net';
+    // ? 'https://localhost:5001'
+    // : 'http://dev-vm-api.citwdd.net';
 
-    if (window.location.hostname.includes('vmfrontend-dev')) {
+
+    if (window.location.hostname.includes('rancher-vmfrontend-dev')) {
+        apiRoot = 'https://rancher-dev-vm-api.citwdd.net';
+    } else if (window.location.hostname.includes('rancher-vmfrontend-test')) {
+        apiRoot = 'https://rancher-test-vm-api.citwdd.net';
+    } else if (window.location.hostname.includes('vmfrontend-dev')) {
         apiRoot = 'https://dev-vm-api.citwdd.net';
     } else if (window.location.hostname.includes('vmfrontend-test')) {
         apiRoot = 'https://test-vm-api.citwdd.net';
     } else if (window.location.hostname.includes('vmfrontend-prod')) {
         apiRoot = 'https://prod-vm-api.citwdd.net';
-    } else if (window.location.hostname.includes('rancher-vmfrontend-dev')){
-        apiRoot = 'https://rancher-dev-vm-api.citwdd.net';
-    }else if (window.location.hostname.includes('rancher-vmfrontend-test')) {
-         apiRoot = 'https://rancher-test-vm-api.citwdd.net';
     }
-    return apiRoot
+return apiRoot
 }
 // export GetApiRoot;
