22965d062e6ae6d831f4513366573c76a4abee2f
Shae Carnahan <car16037@byui.edu>
2022-06-14 09:23:26 -0600

    adding the components to the App.js page

--- vima-react/src/App.js
+++ vima-react/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import {useState} from 'react';
-// import FacultyDashboard from './FacultyDashboard';
-// import StudentDashboard from './StudentDashboard';
+import {useState, useRef} from 'react';
+import FacultyDashboard from './FacultyDashboard';
+import StudentDashboard from './StudentDashboard';
 
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { GoogleLogin } from '@react-oauth/google';
@@ -10,40 +10,31 @@ import jwt_decode from 'jwt-decode';
 const handleFailure = (result) => {
     console.log('There was a problem logging in.', result);
   };
+const isAdmin = [
+    'shaecarnahan@gmail.com',
+    'danieluribe@gmail.com'
+    ];
 
 function App() {
 
 
   const [userIsLoggedIn, setUserLoggedIn] = useState(false);//this creates a placeholder for the user logged in state
   // let userIsAdministrator = useRef(false);//this is similar to state but won't re-render
-  
+  const googleCredentials = useRef({});
   const handleLogin = (googleData) => {
-    const googleCredentials=jwt_decode(googleData.credential);
-    const email = googleCredentials.email;
+    googleCredentials.current = jwt_decode(googleData.credential);
 
-    const isAdmin = [
-    'shaecarnahan@gmail.com',
-    'danieluribe@gmail.com'
-    ];
+    const email = googleCredentials.current.email;
+
+    
   
     setUserLoggedIn(true);
     // userIsAdministrator=true;//we will need to change this to look up the user from the backend
     //this is dummy information on where the page should load next. We would just need to enter a link that we want to go to here!
     console.log(`Welcome ${email} You successfully logged in.`, googleData);
     
-    if(isAdmin.includes(googleCredentials.email)){
-      return (//View could work instead of div here, but not sure  
-          // <FacultyDashboard></FacultyDashboard>
-        window.location.href="VMfaculty_dashboard/facultyview.html"
-      )
-      } 
-      else {
-        return (
-          window.location.href="VMstudent_dashboard/studentview.html"
-            // <StudentDashboard></StudentDashboard>
-        )
-      }
-    };
+  } 
+    
   
   if (!userIsLoggedIn){
 
@@ -98,9 +89,20 @@ function App() {
     </div>
   );
 } else{
+    console.log(googleCredentials.current.email);
+   if(isAdmin.includes(googleCredentials.current.email)){
+      return (//View could work instead of div here, but not sure  
+          <FacultyDashboard></FacultyDashboard>
+        // window.location.href="VMfaculty_dashboard/facultyview.html"
+      )
+      } 
+      else {
+        return (
+          // window.location.href="VMstudent_dashboard/studentview.html"
+            <StudentDashboard></StudentDashboard>
+        )
+      }}
+}
 
-  
-}
-}
 
 export default App;
--- vima-react/src/FacultyDashboard.js
+++ vima-react/src/FacultyDashboard.js
@@ -1,6 +1,63 @@
 const FacultyDashboard = ()=>{
 return(
-    <div>Hello Faculty Dashboard</div>
+    // window.location.href="VMfaculty_dashboard/facultyview.html";
+<div>
+    <div class="container">
+        <img class="logo" src="/images/LOGO-VIMA.png" alt="logo"/>
+        <div class="professorgrid">
+        <span class="material-icons">
+        person
+        </span>
+        <p id="welcome">Welcome Professor |</p>
+
+        <button class="signOut">Sign out</button>
+    </div>
+    <h1 id="name0fProfessor">Hello <span>Professor</span></h1>
+    <p id="greeting">How can we help you today?</p>
+    <div class="addgrid">
+      {/* <div onclick="location='/VMregistration_course_form/addclass.html'" class="add_class"> */}
+        <span class="material-icons">
+          add_circle_outline
+          </span>
+        <button class="submitBt">Add Class</button>
+    </div>
+      <div onclick="location='/VMAdmin_dash/addprofessor.html'" class="add_professor">
+        <span class="material-icons">
+          person_add_alt_1
+        </span>
+        <button class="submit_professor">Add Professors</button>
+      </div>
+      <div onclick="location='/vmutilization/utilization.html'" class="vm_utilization">
+        <span class="material-icons">
+          dns
+        </span>
+        <button class="vm_utilization_text">VM Utilization</button>
+      </div>
+    </div>
+        {/* <!-- <a href="/fetch.html" class="submitBt btn-primary">Courses</a> --> */}
+
+    {/* <!-- <div id="admin_portal"> --> */}
+    {/* </div> */}
+
+
+    <script src="https://apis.google.com/js/platform.js?onload=onLoad" async defer></script>
+
+
+  <div class="area" >
+    <ul class="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+    </ul>
+</div>
+</div>
 )
 }
 
