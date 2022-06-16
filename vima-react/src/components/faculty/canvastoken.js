86f29b6af74c65ba98b9ca011cd1b60d45060b54
Shae Carnahan <car16037@byui.edu>
2022-06-16 10:35:01 -0600

    adding css to components

--- vima-react/src/components/faculty/canvastoken.js
+++ vima-react/src/components/faculty/canvastoken.js
@@ -1,5 +1,5 @@
 import React from "react";
-import '../../Background.css';
+import '../../background.css';
 
 function CanvasToken() {
     return (
--- vima-react/src/components/student/createvm.jsx
+++ vima-react/src/components/student/createvm.jsx
@@ -1,5 +1,5 @@
 import Header from "../components/header";
-import '../../Background.css';
+import '../../background.css';
 
 function CreateVM() {
     return (
