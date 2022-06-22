d7155a436c3c03e6e5be8f0eb97ad0f98160981c
Daniel Uirbe <danielu35@gmail.com>
2022-06-22 12:16:55 -0600

    Fixed the css in the add class css

--- vima-react/src/components/faculty/addclass.js
+++ vima-react/src/components/faculty/addclass.js
@@ -1,33 +1,48 @@
 import React from 'react'
 import Background from '../../background';
 import './addclassdependencies.css';
-import './addclass.module.css'; 
+import style from './addclass.module.css'; 
 import { useNavigate } from 'react-router-dom';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import DnsIcon from '@mui/icons-material/Dns';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import PersonIcon from '@mui/icons-material/Person';
 
 function AddClass() {
   let navigate = useNavigate();
 
   return (
     <>
-    <div class="container">
-    <div id="form-header">
-      <span onClick={() => {navigate("/")}} id="back-btn">&#8592; back</span>
+    <div className={style.container}>
+      <div className={style.header}>
+        <img className={style.logo} src="/images/LOGO-VIMA.png" alt="logo"/>
+        <div className={style.professorgrid}>
+        <span className={style.materialicons}>
+            <PersonIcon sx={{fontSize: '2rem', paddingLeft: "10px"}} />
+        </span>
+        <p id={style.welcome}>Welcome Professor |</p>
+
+        <button className={style.signOut}>Sign Out</button>
+        </div>
+    </div>
+    <div id={style.formheader}>
+      <span onClick={() => {navigate("/")}} id={style.backbtn}>&#8592; back</span>
       <h1>Add Class</h1>
     </div>
-    <div id="grid-cont">
+    <div id={style.gridcont}>
     
       {/* <!-- Course Name--> */}
-        <div class="column1">
+        <div className={style.column1}>
           {/* <p class="validation">Required *</p> */}
-        <label for="name">Course Name:</label>
+        <label className={style.label} for="name">Course Name:</label>
         
 
-        <input type="text" id="name" name="CourseName" placeholder="Enter your class name" required />
+        <input className={style.input} type="text" id={style.name} name="CourseName" placeholder="Enter your class name" required />
 
         {/* <!-- Section course --> */}
 
-        <label>Course Section:</label>
-          <select name="section" id="section" required>
+        <label className={style.label}>Course Section:</label>
+          <select name="section" id={style.section} required>
             <option name="option" value="">Select a section number</option>
             <option name="option" value="1">1</option>
             <option name="option" value="2">2</option>
@@ -36,24 +51,37 @@ function AddClass() {
             <option name="option" value="5">5</option>
           </select>
 
-         
+             {/* <!--Folders  --> */}
+     <label className={style.label}>Choose a Folder:</label>
+     <select name="folder"  id="folder" required > 
+     <option name="option" value="Default">Default</option>
+      </select>
         </div>
 
-        <div class=" column2">
-        <label for="name">Canvas Token:</label>
-        <input type="text" id="canvasToken"  name="canvas_token" placeholder="Enter your class token" required/> 
+        <div className={style.column2}>
+        <label className={style.label} for="name">Canvas Token:</label>
+        <input className={style.input} type="text" id="canvasToken"  name="canvas_token" placeholder="Enter your class token" required/> 
 
 {/* <!--Course Id--> */}
-<label for="name">Canvas Course ID:</label>
+<label className={style.label} for="name">Canvas Course ID:</label>
         <span role="alert" id="nameError" aria-hidden="true">
-        Please add a valid CourseID
+        {/* Please add a valid CourseID */}
         </span>
-        <input type="text" id="courseId" name="CourseId" placeholder="Enter your course Id" required />
+        <input className={style.input} type="text" id="courseId" name="CourseId" placeholder="Enter your course Id" required />
 
+      {/* <!-- Content Library  --> */}
+   <label className={style.label}>Choose a Content Library:</label>
+     <select name="contentLibrary"  id="contentLibrary" required > 
+     <option name="option" value="Default">Default</option>
+      </select>
         </div>
   
+
+
+        
   </div>
-      <button type="button" id="submit" class="save submitBt btn-primary">
+
+      <button type="button" id="submit" className={style.btnprimary}>
         Add
       </button>
     </div>
