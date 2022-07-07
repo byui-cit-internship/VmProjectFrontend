887038d502c7b89a27953a01c3fb7e46309544c5
Shae Carnahan <car16037@byui.edu>
2022-07-07 09:53:40 -0600

    changes to student dash

--- vima-react/src/components/faculty/addvm.js
+++ vima-react/src/components/faculty/addvm.js
@@ -1,6 +1,6 @@
 import React from "react"; 
 import Background from "../../background";
-// import Header from "../../header";
+import Header from "../../header";
 import addvm from './addvm.module.css';
 
 function AddVm() {
@@ -8,7 +8,7 @@ function AddVm() {
         <div className={addvm.addvm}>
         <div className={addvm.container}>
             <div className={addvm.header}>
-                {/* <Header /> */}
+                <Header />
             </div>
             <h1>Add VSphere Template</h1>
             <div className={addvm.content}>
@@ -86,11 +86,10 @@ function AddVm() {
 
             </div>
                 {/* Button to open a modal to add more templates */}
-                    <button id={addvm.open}>Add</button>
+                    <button id={addvm.addtemplate}>Add</button>
         </div>
         <Background/>
         </div>
     )        
 }
-
 export default AddVm;
