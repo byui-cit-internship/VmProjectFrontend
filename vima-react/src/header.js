890483556e5ea40e467f6d1e361d8ed9aac4208f
cindycas <cha19032@byui.edu>
2022-09-14 11:01:29 -0600

    CSS changes (mainly)
    Professor List Page CSS changes

--- vima-react/src/header.js
+++ vima-react/src/header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import header from './header.module.css';
+import professorlist from './components/faculty/professorlist.js';
 import PersonIcon from '@mui/icons-material/Person';
 import { useNavigate } from 'react-router-dom';
 
@@ -8,10 +9,10 @@ function Header() {
     return(
         <div className={header.header}>
             <div className={header.grid}>
-            <img className={header.logo} src="/images/LOGO-VIMA.png" alt="logo" />
-            <PersonIcon className={header.material} />
-            <p id={header.welcome}>Welcome |</p>
-            <button className={header.signOut} onClick={() => {navigate("/")}}>Sign out</button>
+                <img className={header.logo} src="/images/LOGO-VIMA.png" alt="logo" />
+                        <PersonIcon className={header.material} />
+                        <p id={header.welcome}>Welcome |</p>
+                    <button className={header.signOut} onClick={() => {navigate("/")}}>Sign out</button>
             </div>
         </div>
     )
