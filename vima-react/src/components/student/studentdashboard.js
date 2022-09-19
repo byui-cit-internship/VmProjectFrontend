c12b83942b83a49d691f47a3a25df154662be815
mlutz06 <madison.lutz@sentinelofsafety.com>
2022-09-19 16:37:53 -0600

    fixed css

--- vima-react/src/components/student/studentdashboard.js
+++ vima-react/src/components/student/studentdashboard.js
@@ -10,33 +10,47 @@ const iconStyles = {
     color: 'white', fontSize: '35px'
 }
 
+
+
 const StudentDashboard = () => {
   let navigate = useNavigate();
+
+  //userInfo gets user info from token put in session storage to display the users first and last name
+  const userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
+  var userFirst = "";
+  var userLast = "";
+  if (userInfo == null) {
+    userFirst = "Student";
+  } else {
+    userFirst = userInfo.firstName;
+    userLast = userInfo.lastName;
+  };
+
   return(
   <div className={studentdashboard.studentdashboard}>
       <div className={studentdashboard.container}>
         <div className={studentdashboard.header} >
         <Header />  
         </div>
-      <h1 id={studentdashboard.nameOfStudent}>Hello <span>Student</span></h1>
+      <h1 id={studentdashboard.nameOfStudent}>Hello <span>{userFirst} {userLast}</span></h1>
       <p id={studentdashboard.greeting}>How can we help you today?</p>
-        <div className={studentdashboard.createvm}>
+        <div className={studentdashboard.createvm} onClick={() => {navigate("/createvm")}}>
           <span className={studentdashboard.material}>
             <AddCircleOutlineIcon studentdashboard={iconStyles} className={studentdashboard.material}/>
           </span>
-          <button onClick={() => {navigate("/createvm")}}>Create VM</button>
+          <button >Create VM</button>
         </div>
-        <div className={studentdashboard.vm_utilization}>
+        <div className={studentdashboard.vm_utilization} onClick={() => {navigate("/myvm")}}>
           <span className={studentdashboard.material}>
             <Computer studentdashboard={iconStyles} className={studentdashboard.material}/>
           </span>
-          <button onClick={() => {navigate("/myvm")}}>My VMS</button>
+          <button >My VMS</button>
         </div>
-        <div className={studentdashboard.mynetworks}>
+        <div className={studentdashboard.mynetworks} onClick={() => {navigate("/mynetworks")}}>
           <span className={studentdashboard.material}>
             <CloudQueue studentdashboard={iconStyles} className={studentdashboard.material}/>
           </span>
-          <button onClick={() => {navigate("/mynetworks")}}>My Networks</button>
+          <button >My Networks</button>
         </div>
       </div>
       <script src="https://apis.google.com/js/platform.js?onload=onLoad" async defer></script>
