3b4662a936ff8514b105577bcd3f73f00ad5a6c0
mlutz06 <madison.lutz@sentinelofsafety.com>
2022-09-26 12:05:52 -0600

    fixed modal

--- vima-react/src/components/faculty/addprofessor.js
+++ vima-react/src/components/faculty/addprofessor.js
@@ -5,11 +5,51 @@ import { useNavigate } from "react-router-dom";
 import Header from "../../header";
 import {useState} from "react";
 import { FaCheck } from "react-icons/fa";
+import { getApiRoot } from '../../utils/getApiRoot'
 
 
 function AddProfessor() {
+  const validateForm = async ()=> {
+
+    let allFieldsValid = true;
+    if (firstName.length === 0 || lastName.length === 0 || email.length === 0){
+      allFieldsValid = false;
+    }
+    if (allFieldsValid){
+      const response = await fetch(
+        getApiRoot() + '/api/user/admin/createuser', {
+          method: 'POST',
+          body: JSON.stringify({
+            firstName: firstName,
+            lastName: lastName,
+            email: email
+          }),
+          credentials:'include',
+          headers:{
+            'content-type':'application/json'
+          }
+        }
+      )
+      const responseStatus = await response.status;
+      console.log(JSON.stringify(responseStatus));
+      console.log("Professor Added");
+      if (responseStatus === 200) {
+        setIsOpen(true);
+        //This makes the modal window popup
+      }
+      else {
+        console.log("Error", responseStatus);
+      }
+  }
+};
+
   let navigate = useNavigate();
-  const [isOpen, setIsOpen] = useState(false);
+  let [isOpen, setIsOpen] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+
+
   return (
     <div className={addprofessor.addprofessor}>
       <div className={addprofessor.container}>
@@ -35,6 +75,7 @@ function AddProfessor() {
               name="firstName"
               placeholder="Enter professor first name"
               required
+              onChange={(event)=>setFirstName(event.target.value)}
             />
             <label for="name">Last Name:</label>
             <input
@@ -43,6 +84,7 @@ function AddProfessor() {
               name="lastName"
               placeholder="Enter professor last name"
               required
+              onChange={(event)=>setLastName(event.target.value)}
             />
             <label for="name">Email:</label>
             <input
@@ -51,10 +93,11 @@ function AddProfessor() {
               name="email"
               placeholder="Enter professor email"
               required
+              onChange={(event)=>setEmail(event.target.value)}
             />
           </div>
           <img alt="teacher" src="/images/teacherpic.jpg"></img>
-          <button type="submit" className={addprofessor.primaryButton} onClick={()=> setIsOpen(true)}>
+          <button type="submit" className={addprofessor.primaryButton} onClick={()=> validateForm}>
             Add Professor
           </button>
          {isOpen && <div className={addprofessor.modal}>
