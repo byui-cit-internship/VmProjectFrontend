ddc072360d5a5d2aefdb242e15ff6718581080a8
cindycas <cha19032@byui.edu>
2022-10-11 11:27:20 -0600

    Revert "Delete router.js"
    This reverts commit 8e2e19f48a82403996b6b3b09368847f329a3aa7.

---
+++ vima-react/src/router.js
@@ -0,0 +1,37 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./app";
+import FacultyDashboard from "./components/faculty/facultydashboard";
+import AddClass from "./components/faculty/addclass";
+import AddProfessor from "./components/faculty/addprofessor";
+import AddVm from "./components/faculty/addvm";
+import StudentDashboard from "./components/student/studentdashboard";
+import CreateVM from "./components/student/createvm";
+import MyVM from "./components/student/myvm";
+import Utilization from "./components/faculty/utilization";
+import MyNetworks from "./components/student/mynetworks";
+import ProfessorList from "./components/faculty/professorlist";
+import CreatedSuccessfully from "./components/student/success";
+import ClassList from "./components/faculty/classlist";
+
+export default function VimaRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/faculty" element={<FacultyDashboard />} />
+        <Route path="/addclass" element={<AddClass />} />
+        <Route path="/addprofessor" element={<AddProfessor />} />
+        <Route path="/utilization" element={<Utilization />} />
+        <Route path="/addvm" element={<AddVm />} />
+        <Route path="/student" element={<StudentDashboard />} />
+        <Route path="/createvm" element={<CreateVM />} />
+        <Route path="/myvm" element={<MyVM />} />
+        <Route path="/mynetworks" element={<MyNetworks />} />
+        <Route path="/professorlist" element={<ProfessorList />} />
+        <Route path="/success" element={<CreatedSuccessfully />} />
+        <Route path="/classlist" element={<ClassList />} />
+        {/* <Route path="/" element={<ClassList />} /> */}
+      </Routes>
+    </Router>
+  );
+}
