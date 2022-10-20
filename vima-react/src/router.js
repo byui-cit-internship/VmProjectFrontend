3e619143df8d46e07823e0dcb6dbf0352017b449
cindycas <cha19032@byui.edu>
2022-10-20 14:57:06 -0600

    verifyemail page added
    not working at all btw

--- vima-react/src/router.js
+++ vima-react/src/router.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from './app';
+import App from "./app";
 import FacultyDashboard from "./components/faculty/facultydashboard";
 import AddClass from "./components/faculty/addclass";
 import AddProfessor from "./components/faculty/addprofessor";
@@ -12,26 +12,28 @@ import MyNetworks from "./components/student/mynetworks";
 import ProfessorList from "./components/faculty/professorlist";
 import CreatedSuccessfully from "./components/student/success";
 import MyClasses from "./components/faculty/myclasses";
+import VerifyEmail from "./verifyemail";
 import Popup from "./components/faculty/Popup";
 
-export default function VimaRouter () {
-    return (
-        <Router>
-            <Routes>
-                <Route path='/' element={<App />} />
-                <Route path='/faculty' element={<FacultyDashboard />} />
-                <Route path='/addclass' element={<AddClass />} />
-                <Route path='/addprofessor' element={<AddProfessor />} />
-                <Route path='/utilization' element={<Utilization />} />
-                <Route path='/addvm' element={<AddVm />} />
-                <Route path='/student' element={<StudentDashboard />} />
-                <Route path='/createvm' element={<CreateVM />} />
-                <Route path='/myvm' element={<MyVM />} />
-                <Route path='/mynetworks' element={<MyNetworks />} />
-                <Route path='/professorlist' element={<ProfessorList />} />
-                <Route path='/success' element={<CreatedSuccessfully />} />
-                <Route path='/myclasses' element={<MyClasses />} />
-            </Routes>
-        </Router>
-    )
+export default function VimaRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/faculty" element={<FacultyDashboard />} />
+        <Route path="/addclass" element={<AddClass />} />
+        <Route path="/addprofessor" element={<AddProfessor />} />
+        <Route path="/utilization" element={<Utilization />} />
+        <Route path="/addvm" element={<AddVm />} />
+        <Route path="/student" element={<StudentDashboard />} />
+        <Route path="/createvm" element={<CreateVM />} />
+        <Route path="/myvm" element={<MyVM />} />
+        <Route path="/mynetworks" element={<MyNetworks />} />
+        <Route path="/professorlist" element={<ProfessorList />} />
+        <Route path="/success" element={<CreatedSuccessfully />} />
+        <Route path="/myclasses" element={<MyClasses />} />
+        <Route path="/verifyemail" element={<VerifyEmail />} />
+      </Routes>
+    </Router>
+  );
 }
