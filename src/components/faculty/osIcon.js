6f21ca8ae8ef3787fdbc2856c1cb79385c57db57
Daniel De La Pena <dadelapema@gmail.com>
2022-12-05 14:37:13 -0700

    styled icons

--- src/components/faculty/osIcon.js
+++ src/components/faculty/osIcon.js
@@ -2,27 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 import { BsQuestionLg } from "react-icons/bs";
 import { getOSIcon } from "react-os-icons";
-import addvm from "./addvm.module.css";
+import osIcon from "./osIcon.module.css";
 
-const WindowsIcon = getOSIcon({ os: "Windows", className: addvm.icon });
-const UbuntuIcon = getOSIcon({ os: "Ubuntu", className: addvm.icon });
-const DebianIcon = getOSIcon({ os: "Debian", className: addvm.icon });
-const LinuxIcon = getOSIcon({ os: "Linux", className: addvm.icon });
-const MacIcon = getOSIcon({ os: "Mac OS", className: addvm.icon });
-const FedoraIcon = getOSIcon({ os: "Fedora", className: addvm.icon });
-const LinuxMintIcon = getOSIcon({ os: "Linux Mint", className: addvm.icon });
+const WindowsIcon = getOSIcon({ os: "Windows", className: osIcon.windowsIcon });
+const UbuntuIcon = getOSIcon({ os: "Ubuntu", className: osIcon.ubuntuIcon });
+const DebianIcon = getOSIcon({ os: "Debian", className: osIcon.debianIcon });
+const LinuxIcon = getOSIcon({ os: "Linux", className: osIcon.linuxIcon });
+const MacIcon = getOSIcon({ os: "Mac OS", className: osIcon.macIcon });
+const FedoraIcon = getOSIcon({ os: "Fedora", className: osIcon.fedoraIcon });
+const LinuxMintIcon = getOSIcon({ os: "Linux Mint", className: osIcon.linuxIcon });
 
 const OSIcon = (props) => {
   const osName = props.operatingSystem.toUpperCase();
 
   if (osName.includes("WINDOWS")) return WindowsIcon;
   if (osName.includes("MAC")) return MacIcon;
-  if (osName.includes("UBUNTU")) return UbuntuIcon;
+  if (osName.includes("UBUNTU")) return LinuxMintIcon;
   if (osName.includes("DEBIAN")) return DebianIcon;
   if (osName.includes("FEDORA")) return FedoraIcon;
   if (osName.includes("MINT")) return LinuxMintIcon;
   if (osName.includes("LINUX")) return LinuxIcon;
-  return <BsQuestionLg className={addvm.questionMark} />;
+  return <BsQuestionLg className={osIcon.questionMark} />;
 };
 
 OSIcon.propTypes = {
