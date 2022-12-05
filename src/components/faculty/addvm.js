57c2a1887b0e4cb8faf5ec49b7e9fb5a998a85fb
Daniel De La Pena <del17011@byui.edu>
2022-12-05 10:08:18 -0700

    more

--- src/components/faculty/addvm.js
+++ src/components/faculty/addvm.js
@@ -6,6 +6,16 @@ import Apple from "@mui/icons-material/Apple";
 import { getApiRoot } from "../../utils/getApiRoot";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import TemplateMetadata from "./templateMetadata";
+import { getOSIcon } from "react-os-icons";
+
+const WindowsIcon = getOSIcon({ os: "Windows", className: "icon windows" });
+const UbuntuIcon = getOSIcon({ os: "Ubunty", className: "icon ubuntu" });
+const DebianIcon = getOSIcon({ os: "Debian", className: "icon debian" });
+const LinuxIcon = getOSIcon({ os: "Linux", className: "icon linux" });
+const MacIcon = getOSIcon({ os: "Mac OS", className: "icon mac" });
+const FedoraIcon = getOSIcon({ os: "Fedora", className: "icon fedora" });
+const ArchIcon = getOSIcon({ os: "Arch", className: "icon arch" });
+const LinuxMintIcon = getOSIcon({ os: "Linux Mint", className: "icon linux-mint" });
 
 function AddVm() {
   const [libraryId, setLibraryId] = useState();
@@ -70,10 +80,6 @@ function AddVm() {
     }
   }, [libraryId]);
 
-  const handleSubmission = (template) => {
-    console.log("template: ", template);
-  };
-
   return (
     <div className={addvm.addvm}>
       <div className={addvm.container}>
@@ -86,37 +92,44 @@ function AddVm() {
           <div className={addvm.contentLibrary}>
             <label>Content Library:</label>
             <input
+              className={addvm.inputBox}
               name="contentLibrary"
               id={addvm.contentLibrary}
               value={library ? library.name : "-No Libraries Found-"}
               disabled
             />
           </div>
-
           {/* Virtual Machine */}
           <div className={addvm.chooseVm}>
-            <h3>Choose a Virtual Machine Template:</h3>
             {templates && (
               <div>
-                {templates && (
-                  <div
-                    onChange={(e) => {
-                      setTemplate(e.target.value);
-                    }}>
-                    {templates.map((template) => (
-                      <label key={template.id}>
-                        <input type="radio" name="template" value={JSON.stringify(template)} />
-                        {template.name}
-                      </label>
-                    ))}
-                  </div>
-                )}
-                <TemplateMetadata templateInfo={template} key={template}/>
+                <div className={addvm.templateOptionsContainer}>
+                  <h3 className={addvm.headerText}>Choose a Virtual Machine Template:</h3>
+                  {templates && (
+                    <div
+                      className={addvm.templates}
+                      onChange={(e) => {
+                        setTemplate(e.target.value);
+                      }}>
+                      {templates.map((template) => (
+                        <div className={addvm.flexbox}>
+                          <label key={template.id} className={addvm.template}>
+                            <input type="radio" name="template" value={JSON.stringify(template)} />
+                            {WindowsIcon}
+                          </label>
+                          <div>{template.name}</div>
+                        </div>
+                      ))}
+                    </div>
+                  )}
+                </div>
+                <TemplateMetadata templateInfo={template} key={template} />
               </div>
             )}
           </div>
+          <button id={addvm.open}>Add</button>
         </div>
-        <button id={addvm.open}>Add</button>
+
         <Background templateInfo={template} />
       </div>
     </div>
